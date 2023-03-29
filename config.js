eea3ef4b03922841f3d8b124ee32c26ef52d184e
erik <uckert@outlook.com>
2023-03-29 13:17:50 +0200

    delete unused config file

--- config.js
+++
@@ -1,8 +0,0 @@
-// config.js
-const dotenv = require('dotenv');
-dotenv.config();
-module.exports = {
-  apikey: process.env.APIKEY,
-  organisation: process.env.ORGANISATION,
-  port: process.env.PORT
-};
